81c36b0a55d7364f28f8cfc18e1cbaa264ab91ef
JamalDunn23 <jamaldunn23@gmail.com>
2023-07-01 13:44:15 -0400

    Remove Button Added

--- main.js
+++ main.js
@@ -7,19 +7,44 @@ form.addEventListener("submit", (e) => {
   let li = document.createElement("li");
   let name = form.querySelector("#name").value;
   let price = form.querySelector("#price").value;
-  let inStock = form.querySelector("#stock").value;
+  let inStockCheckbox = form.querySelector("#remove-check-box").value;
+  let inStock = inStockCheckbox.checked
   let image = form.querySelector("#img").value;
   let shirtColor = form.querySelector("#shirt-color").value;
 
-  li.innerHTML = `<h3>Name: </h3> <p> ${name} </p>
-  <h3>Price:<h3/> <p>${price}<p>
-  <h3>In Stock:</h3> <p>${inStock}</p>
+  li.innerHTML = `
+  <img src = ${image} alt="image" /> 
+  <h3>Name: </h3> <p> ${name} </p>
+<h3>Price:<h3/> <p>${price}<p>
+  <h3>In Stock:</h3><p>${inStock ? '<input type="checkbox" checked disabled />' : '<input type="checkbox" disabled />'}</p>
+  <input type="checkbox" class="remove-check-box"/>
 <h3>Color:<h3/><p>${shirtColor}</p>
-  <img src = ${image} alt="image" /> `;
+<br/>
+  <button class="removeBtn"> Remove <button/> `;
+
 
   ul.append(li);
+
+
+  const deleteButton = li.querySelector(".removeBtn");
+  deleteButton.addEventListener("click", () => {
+    li.remove();
+  });
+
+  form.reset()
 });
 
-form.addEventListener("delete", (e) => {
-  e.preventDefaulr();
+let removeBtns = document.querySelectorAll(".removeBtn");
+
+removeBtns.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    console.log("this is working");
+    const li = e.target.closest("li");
+    li.remove();
+  });
+//   const removeCheckboxes = document.querySelectorAll(".remove-check-box”);
+removeCheckboxes.forEach((checkbox) => {
+  checkbox.addEventListener("change", () => {
+    checkbox.checked = !checkbox.checked;
+  });
 });
